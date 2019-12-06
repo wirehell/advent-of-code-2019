a3d08f7a902ae8fa1b385e9ec7e5fa9023c89dbe
Kenny Johansson <wirehell@gmail.com>
2019-12-06 14:15:22 +0100

    Day 5 part 1

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,27 +1,114 @@
 use std::env;
+use crate::Instruction::{Halt, Add, Multiply, Input, Output};
+use crate::Parameter::{Imm, Pos};
+
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+type Memory = Vec<i32>;
+type IP = usize;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Instruction {
+    Add {op1 :Parameter, op2: Parameter, dst: Parameter},
+    Multiply {op1 :Parameter, op2: Parameter, dst: Parameter},
+    Input {dst :Parameter},
+    Output {src :Parameter},
+    Halt,
+}
+
+fn instruction_size(instruction :&Instruction) -> usize {
+    return match instruction {
+        Add { .. } => 4,
+        Multiply { .. } => 4,
+        Input { .. } => 2,
+        Output { .. } => 2,
+        Halt => 1,
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Parameter {
+    Pos(usize),
+    Imm(i32),
+}
+
+fn read_params(ip: IP, memory :&Memory, num: i32) -> Vec<Parameter> {
+    let mut params: Vec<Parameter> = vec![];
+    let mut param_code = memory[ip] / 100;
+    for i in 1..=num {
+        let val = memory[ip + i as usize];
+        let p = match param_code % 10 {
+            0 => Parameter::Pos(val as usize),
+            1 => Parameter::Imm(val),
+            _ => panic!("Invalid param: {}", memory[ip]/100),
+        };
+        param_code /= 10;
+        params.push(p)
+    }
+    return params;
+}
+
+fn decode_instruction(ip: IP, memory :&Memory) -> Instruction {
+    let op_code = memory[ip] % 100;
+    return match op_code {
+        1 => {
+            let params = read_params(ip, &memory, 3);
+            Add { op1: params[0], op2: params[1], dst: params[2] }
+        },
+        2 => {
+            let params = read_params(ip, &memory, 3);
+            Multiply { op1: params[0], op2: params[1], dst: params[2] }
+        }
+        3 => {
+            let params = read_params(ip, &memory, 1);
+            Input {dst: params[0]}
+        }
+        4 => {
+            let params = read_params(ip, &memory, 1);
+            Output {src: params[0]}
+        }
+        99 => Halt,
+
+        _ => panic!("Unknown instruction: {:?}", memory[ip]),
+
+    };
+}
+
+fn write(memory :&mut Memory, val: i32, dst: Parameter) {
+    match dst {
+        Imm(_)   => panic!("Writing IMM"),
+        Pos(p) => {
+            //println!("Write: mem[{}] = {}", p, val);
+            memory[p] = val;
+        },
+    }
+}
+
+fn load(memory :&Memory, src: Parameter) -> i32 {
+    match src {
+        Imm(v) => {
+            //println!("Load const: {}", v);
+            return v;
+        }
+        Pos(p) => {
+            let val = memory[p];
+            //println!("Loaded: {} <- mem[{}]", val, p);
+            return val;
+        },
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     println!("Reading from file: {}", filename);
     let s = std::fs::read_to_string(filename).unwrap();
-
     let sl = split_and_parse(&s);
-    let result = find(19690720, sl);
-    println!("Result: {}", result);
-
-}
-
-fn find(result:i32, initial:Vec<i32>) -> i32 {
-    for noun in 0..255 {
-        for verb in 0..173 {
-            let r = run(noun, verb, initial.clone());
-            if r[0] == result {
-                return 100*noun + verb;
-            }
-        }
-    }
-    panic!("Not found");
+    let result = execute(sl);
 }
 
 fn run(noun:i32, verb:i32, initial:Vec<i32>) -> Vec<i32> {
@@ -36,46 +123,73 @@ fn split_and_parse(s :&str) -> Vec<i32> {
    return split.map(|x| x.parse::<i32>().unwrap()).collect();
 }
 
-fn execute(initial: Vec<i32>) -> Vec<i32> {
+fn execute(initial: Memory) -> Memory {
+    //println!("Starting new execution");
     let mut ip = 0;
-    let mut state = initial;
+    let mut mem = initial;
     loop {
-        let op_code = state[ip as usize];
-        if op_code == 99 {
-            break;
-        }
-        match op_code {
-            1 => {
-                // dst = op1 + op2
-                let op1 = state[ip + 1];
-                let op2 = state[ip + 2];
-                let dst = state[ip + 3];
-                state[dst as usize] = state[op1 as usize] + state[op2 as usize];
-                ip += 4;
+        //println!("Executing at IP:{}", ip);
+        let instruction = decode_instruction(ip, &mem);
+        match instruction {
+            Add {op1, op2, dst} => {
+                let v1 = load(&mem, op1);
+                let v2 = load(&mem, op2);
+                let res = v1 + v2;
+                write(&mut mem, res, dst);
+            },
+            Multiply {op1, op2, dst} => {
+                let v1 = load(&mem, op1);
+                let v2 = load(&mem, op2);
+                let res = v1 * v2;
+                write(&mut mem, res, dst);
+            },
+            Input {dst} => {
+                write(&mut mem, input(), dst)
+            },
+            Output {src} => {
+                output(load(&mem, src))
             }
-            2 => {
-                // dst = op1 * op2
-                let op1 = state[ip + 1];
-                let op2 = state[ip + 2];
-                let dst = state[ip + 3];
-                state[dst as usize] = state[op1 as usize]* state[op2 as usize];
-                ip += 4;
-            }
-            _ => panic!(),
-
+            Halt => {
+                println!("Halt!");
+                return mem;
+            },
+            _ => panic!("Unknown instruction: {}", ip),
         }
+        ip += instruction_size(&instruction);
     }
-    return state;
 }
 
+fn input() -> i32 {
+   return 1;
+}
+
+fn output(val :i32) {
+    println!("Output: {}", val)
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{find, execute, split_and_parse};
+    use crate::{execute, split_and_parse, decode_instruction};
+    use crate::Instruction::{Add, Multiply};
+    use crate::Parameter::{Imm, Pos};
+
+    fn init() {
+        let _ = env_logger::builder()
+            .is_test(true)
+            .try_init();
+    }
 
     #[test]
     fn test() {
         assert_eq!(split_and_parse(&"1,2,3"), [1,2,3])
+    }
+
+    #[test]
+    fn testDecode() {
+        assert_eq!(decode_instruction(0, &vec![1,0,0,0,99]),
+                   Add {op1: Pos(0), op2: Pos(0), dst: Pos(0) });
+        assert_eq!(decode_instruction(0, &vec![1002,4,3,4,33]),
+                   Multiply {op1: Pos(4), op2: Imm(3), dst: Pos(4) });
     }
 
     #[test]
@@ -87,9 +201,9 @@ mod tests {
     }
 
     #[test]
-    fn testFind() {
-        assert_eq!(find(10566835, vec![1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,6,19,23,2,6,23,27,1,5,27,31,2,31,9,35,1,35,5,39,1,39,5,43,1,43,10,47,2,6,47,51,1,51,5,55,2,55,6,59,1,5,59,63,2,63,6,67,1,5,67,71,1,71,6,75,2,75,10,79,1,79,5,83,2,83,6,87,1,87,5,91,2,9,91,95,1,95,6,99,2,9,99,103,2,9,103,107,1,5,107,111,1,111,5,115,1,115,13,119,1,13,119,123,2,6,123,127,1,5,127,131,1,9,131,135,1,135,9,139,2,139,6,143,1,143,5,147,2,147,6,151,1,5,151,155,2,6,155,159,1,159,2,163,1,9,163,0,99,2,0,14,0
-]), 1202)
+    fn testIO() {
+//        assert_eq!(execute(vec![1,0,0,0,99]), [2,0,0,0,99]);
+        execute(vec![3,0,4,0,99]);
     }
 
 }
