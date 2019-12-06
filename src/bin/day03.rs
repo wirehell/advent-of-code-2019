7f26caaed8bbeacb377def32028c01c9ba135bea
Kenny Johansson <wirehell@gmail.com>
2019-12-06 10:38:02 +0100

    Day 3 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -3,6 +3,7 @@ use std::io::{BufReader, BufRead};
 use std::fs::File;
 use regex::Regex;
 use crate::Dir::{Vertical, Horizontal};
+use std::process::exit;
 
 extern crate regex;
 
@@ -17,6 +18,7 @@ struct Line {
     from: Point,
     to: Point,
     dir: Dir,
+    len: i32,
 }
 enum Dir {
     Vertical, Horizontal
@@ -31,6 +33,10 @@ enum Ext {
     R(i32),
 }
 
+fn wire_length(wire: &Wire) -> i32 {
+    return wire.last().map(|l| l.len).unwrap_or(0)
+}
+
 fn extend_wire(wire: &mut Wire, extension: Ext) {
 
     let last_point = wire.last().map_or(ORIGIN, |l| l.to);
@@ -42,13 +48,15 @@ fn extend_wire(wire: &mut Wire, extension: Ext) {
         Ext::L(n) => Point { x: last_point.x - n, y: last_point.y },
     };
 
-    let dir = match extension {
-        Ext::U(_)|Ext::D(_) => Vertical,
-        Ext::R(_) | Ext::L(_) => Horizontal,
+    let (dir, len) = match extension {
+        Ext::U(l)|Ext::D(l) => (Vertical,l),
+        Ext::R(l) | Ext::L(l) => (Horizontal,l)
     };
 
+    let len = wire_length(&wire) + len;
+
 //    println!("NP: {} {}", new_point.x, new_point.y);
-    wire.push(Line {from: last_point, to: new_point, dir });
+    wire.push(Line {from: last_point, to: new_point, dir, len:len });
 }
 
 fn parse_extension(s :&str) -> Ext {
@@ -80,10 +88,30 @@ fn get_intersection_2(h_line: &Line, v_line:&Line) -> Option<Point> {
     return None
 }
 
-fn get_intersection(line1 :&Line, line2:&Line) -> Option<Point> {
+fn get_intersection_distance(h_line: &Line, v_line:&Line) -> Option<i32> {
+    let x = v_line.from.x;
+    let y = h_line.from.y;
+
+    let x_min = i32::min(h_line.from.x, h_line.to.x);
+    let x_max = i32::max(h_line.from.x, h_line.to.x);
+
+    let y_min = i32::min(v_line.from.y, v_line.to.y);
+    let y_max = i32::max(v_line.from.y, v_line.to.y);
+
+    if x > x_min && x < x_max && y > y_min && y < y_max {
+        println!("Before {} {}", h_line.len, v_line.len);
+        println!("x: {} y: {}", x, y);
+        return Some(h_line.len + v_line.len
+            - i32::abs(x - h_line.to.x) - i32::abs(y - v_line.to.y))
+    }
+
+    return None
+}
+
+fn get_intersection(line1 :&Line, line2:&Line) -> Option<i32> {
     return match (&line1.dir, &line2.dir) {
-        (Horizontal, Vertical) => get_intersection_2(line1, line2),
-        (Vertical, Horizontal) =>  get_intersection_2(line2, line1),
+        (Horizontal, Vertical) => get_intersection_distance(line1, line2),
+        (Vertical, Horizontal) =>  get_intersection_distance(line2, line1),
         _ => None
     };
 }
@@ -123,8 +151,8 @@ fn solve(s1 :&str, s2 :&str) -> i32 {
         for l2 in &wire2 {
             match get_intersection(l1, l2) {
                 None => (),
-                Some(point) => {
-                    let d = distance_origin(point);
+                Some(d) => {
+                    println!("dist: {}", d);
                     if d < dist {
                         dist = d;
                     }
@@ -140,9 +168,16 @@ mod tests {
 
     #[test]
     fn test() {
+        /*
+        //part 1
         assert_eq!(solve(&"R8,U5,L5,D3", &"U7,R6,D4,L4"), 6);
         assert_eq!(solve(&"R75,D30,R83,U83,L12,D49,R71,U7,L72", &"U62,R66,U55,R34,D71,R55,D58,R83"), 159);
         assert_eq!(solve(&"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", &"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"), 135);
+        */
+        // Part 2
+        assert_eq!(solve(&"R8,U5,L5,D3", &"U7,R6,D4,L4"), 30);
+        assert_eq!(solve(&"R75,D30,R83,U83,L12,D49,R71,U7,L72", &"U62,R66,U55,R34,D71,R55,D58,R83"), 610);
+        assert_eq!(solve(&"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", &"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"), 410);
     }
 
 }
