9729a753d6c70f1befe40a77770e20816233e195
Kenny Johansson <wirehell@gmail.com>
2019-12-06 15:09:45 +0100

    Day 5

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,13 +1,11 @@
 use std::env;
-use crate::Instruction::{Halt, Add, Multiply, Input, Output};
+
+use crate::Instruction::{Halt, Add, Multiply, Input, Output, JumpIfTrue, JumpIfFalse, LessThan, Equals};
 use crate::Parameter::{Imm, Pos};
 
-
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-
 type Memory = Vec<i32>;
+type OutputData = Vec<i32>;
+type InputData = Vec<i32>;
 type IP = usize;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -16,17 +14,11 @@ enum Instruction {
     Multiply {op1 :Parameter, op2: Parameter, dst: Parameter},
     Input {dst :Parameter},
     Output {src :Parameter},
+    JumpIfTrue {cond: Parameter, target: Parameter},
+    JumpIfFalse {cond: Parameter, target: Parameter},
+    LessThan {op1 :Parameter, op2: Parameter, dst: Parameter},
+    Equals {op1 :Parameter, op2: Parameter, dst: Parameter},
     Halt,
-}
-
-fn instruction_size(instruction :&Instruction) -> usize {
-    return match instruction {
-        Add { .. } => 4,
-        Multiply { .. } => 4,
-        Input { .. } => 2,
-        Output { .. } => 2,
-        Halt => 1,
-    }
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -61,15 +53,31 @@ fn decode_instruction(ip: IP, memory :&Memory) -> Instruction {
         2 => {
             let params = read_params(ip, &memory, 3);
             Multiply { op1: params[0], op2: params[1], dst: params[2] }
-        }
+        },
         3 => {
             let params = read_params(ip, &memory, 1);
             Input {dst: params[0]}
-        }
+        },
         4 => {
             let params = read_params(ip, &memory, 1);
             Output {src: params[0]}
-        }
+        },
+        5 => {
+            let params = read_params(ip, &memory, 2);
+            JumpIfTrue {cond: params[0], target: params[1]}
+        },
+        6 => {
+            let params = read_params(ip, &memory, 2);
+            JumpIfFalse {cond: params[0], target: params[1]}
+        },
+        7 => {
+            let params = read_params(ip, &memory, 3);
+            LessThan {op1: params[0], op2: params[1], dst: params[2]}
+        },
+        8 => {
+            let params = read_params(ip, &memory, 3);
+            Equals {op1: params[0], op2: params[1], dst: params[2]}
+        },
         99 => Halt,
 
         _ => panic!("Unknown instruction: {:?}", memory[ip]),
@@ -108,14 +116,8 @@ fn main() {
     println!("Reading from file: {}", filename);
     let s = std::fs::read_to_string(filename).unwrap();
     let sl = split_and_parse(&s);
-    let result = execute(sl);
-}
-
-fn run(noun:i32, verb:i32, initial:Vec<i32>) -> Vec<i32> {
-    let mut state = initial;
-    state[1] = noun;
-    state[2] = verb;
-    return execute(state);
+    let result = execute_with_result(sl, vec![5]);
+    println!("Result: {:?}", result);
 }
 
 fn split_and_parse(s :&str) -> Vec<i32> {
@@ -123,8 +125,16 @@ fn split_and_parse(s :&str) -> Vec<i32> {
    return split.map(|x| x.parse::<i32>().unwrap()).collect();
 }
 
-fn execute(initial: Memory) -> Memory {
+fn execute_with_result(initial: Memory, in_data: InputData) -> OutputData {
+    let (_, result) =  execute(initial, in_data);
+    return result;
+}
+
+fn execute(initial: Memory, in_data: Vec<i32>) -> (Memory, Vec<i32>)  {
     //println!("Starting new execution");
+    let mut output = vec![];
+    let mut input = in_data;
+
     let mut ip = 0;
     let mut mem = initial;
     loop {
@@ -136,26 +146,71 @@ fn execute(initial: Memory) -> Memory {
                 let v2 = load(&mem, op2);
                 let res = v1 + v2;
                 write(&mut mem, res, dst);
+                ip += 4;
             },
             Multiply {op1, op2, dst} => {
                 let v1 = load(&mem, op1);
                 let v2 = load(&mem, op2);
                 let res = v1 * v2;
                 write(&mut mem, res, dst);
+                ip += 4;
             },
             Input {dst} => {
-                write(&mut mem, input(), dst)
+                write(&mut mem, input.pop().unwrap(), dst);
+                ip += 2;
             },
             Output {src} => {
-                output(load(&mem, src))
+                output.push(load(&mem, src));
+                ip += 2;
+            }
+            JumpIfTrue { cond, target } => {
+                let val = load(&mem, cond);
+                if val != 0 {
+                    let target = load(&mem, target);
+                    ip = target as usize;
+                } else {
+                    ip += 3
+                }
+            }
+            JumpIfFalse { cond, target } => {
+                let val = load(&mem, cond);
+                if val == 0 {
+                    let target = load(&mem, target);
+                    ip = target as usize;
+                } else {
+                    ip += 3
+                }
+            }
+            LessThan { op1, op2, dst } => {
+                let val1 = load(&mem, op1);
+                let val2 = load(&mem, op2);
+                let result;
+                if val1 < val2 {
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+                write(&mut mem, result, dst);
+                ip += 4
+            }
+            Equals { op1, op2, dst } => {
+                let val1 = load(&mem, op1);
+                let val2 = load(&mem, op2);
+                let result;
+                if val1 == val2 {
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+                write(&mut mem, result, dst);
+                ip += 4
             }
             Halt => {
                 println!("Halt!");
-                return mem;
+                return (mem, output);
             },
             _ => panic!("Unknown instruction: {}", ip),
         }
-        ip += instruction_size(&instruction);
     }
 }
 
@@ -169,15 +224,9 @@ fn output(val :i32) {
 
 #[cfg(test)]
 mod tests {
-    use crate::{execute, split_and_parse, decode_instruction};
+    use crate::{execute, split_and_parse, decode_instruction, execute_with_result};
     use crate::Instruction::{Add, Multiply};
     use crate::Parameter::{Imm, Pos};
-
-    fn init() {
-        let _ = env_logger::builder()
-            .is_test(true)
-            .try_init();
-    }
 
     #[test]
     fn test() {
@@ -185,7 +234,7 @@ mod tests {
     }
 
     #[test]
-    fn testDecode() {
+    fn test_decode() {
         assert_eq!(decode_instruction(0, &vec![1,0,0,0,99]),
                    Add {op1: Pos(0), op2: Pos(0), dst: Pos(0) });
         assert_eq!(decode_instruction(0, &vec![1002,4,3,4,33]),
@@ -193,17 +242,54 @@ mod tests {
     }
 
     #[test]
-    fn testExecute() {
-        assert_eq!(execute(vec![1,0,0,0,99]), [2,0,0,0,99]);
-        assert_eq!(execute(vec![2,3,0,3,99]), [2,3,0,6,99]);
-        assert_eq!(execute(vec![2,4,4,5,99,0]), [2,4,4,5,99,9801]);
-        assert_eq!(execute(vec![1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99]);
+    fn test_execute() {
+        assert_eq!(execute(vec![1,0,0,0,99], vec![]), (vec![2,0,0,0,99], vec![]));
+        assert_eq!(execute(vec![2,3,0,3,99], vec![]), (vec![2,3,0,6,99], vec![]));
+        assert_eq!(execute(vec![2,4,4,5,99,0], vec![]), (vec![2,4,4,5,99,9801], vec![]));
+        assert_eq!(execute(vec![1,1,1,4,99,5,6,0,99], vec![]), (vec![30,1,1,4,2,5,6,0,99], vec![]));
     }
 
     #[test]
     fn testIO() {
-//        assert_eq!(execute(vec![1,0,0,0,99]), [2,0,0,0,99]);
-        execute(vec![3,0,4,0,99]);
+        assert_eq!(execute_with_result(vec![3,0,4,0,99], vec![73]), vec![73]);
     }
 
+    #[test]
+    fn test_comparision() {
+        // Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        assert_eq!(execute_with_result(vec![3,9,8,9,10,9,4,9,99,-1,8], vec![8]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,9,8,9,10,9,4,9,99,-1,8], vec![7]), vec![0]);
+        assert_eq!(execute_with_result(vec![3,9,8,9,10,9,4,9,99,-1,8], vec![9]), vec![0]);
+        // Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        assert_eq!(execute_with_result(vec![3,9,7,9,10,9,4,9,99,-1,8], vec![8]), vec![0]);
+        assert_eq!(execute_with_result(vec![3,9,7,9,10,9,4,9,99,-1,8], vec![7]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,9,7,9,10,9,4,9,99,-1,8], vec![9]), vec![0]);
+        // Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        assert_eq!(execute_with_result(vec![3,3,1108,-1,8,3,4,3,99], vec![7]), vec![0]);
+        assert_eq!(execute_with_result(vec![3,3,1108,-1,8,3,4,3,99], vec![8]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,3,1108,-1,8,3,4,3,99], vec![9]), vec![0]);
+        // Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        assert_eq!(execute_with_result(vec![3,3,1107,-1,8,3,4,3,99], vec![7]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,3,1107,-1,8,3,4,3,99], vec![8]), vec![0]);
+        assert_eq!(execute_with_result(vec![3,3,1107,-1,8,3,4,3,99], vec![9]), vec![0]);
+    }
+
+    #[test]
+    fn test_jumps() {
+        // Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+        assert_eq!(execute_with_result(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], vec![9]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], vec![0]), vec![0]);
+        assert_eq!(execute_with_result(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], vec![9]), vec![1]);
+        assert_eq!(execute_with_result(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], vec![0]), vec![0]);
+    }
+
+
+    #[test]
+    fn test_complex() {
+        let program = vec![3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
+
+        assert_eq!(execute_with_result(program.clone(), vec![8]), vec![1000]);
+        assert_eq!(execute_with_result(program.clone(), vec![9]), vec![1001]);
+        assert_eq!(execute_with_result(program.clone(), vec![7]), vec![999]);
+    }
 }
